da7dab0b48089bae3f5b0e61692424db22719e8b
ohyerim025-alt <ohyerim025@gmail.com>
2025-11-04 14:30:52 +0900

    feat: 테이블 생성, 로그인, 로그아웃

---
+++ services/account-service/src/main/java/com/OhRyue/certpilot/account/dto/UserRegisterDto.java
@@ -0,0 +1,17 @@
+package com.OhRyue.certpilot.account.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegisterDto {
+    private String username;
+    private String password;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
