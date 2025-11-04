f8b65f7cdf3bf6877b2a2c2a0a841d3a0e10e1e2
ohyerim025-alt <ohyerim025@gmail.com>
2025-11-04 20:24:17 +0900

    docs: SecurityConfig 주석 추가

--- services/account-service/src/main/java/com/OhRyue/certpilot/account/config/SecurityConfig.java
+++ services/account-service/src/main/java/com/OhRyue/certpilot/account/config/SecurityConfig.java
@@ -8,6 +8,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
+    /*
+        BCryptPasswordEncoder를 스프링 빈으로 등록
+        - 비밀번호를 암호화해서 저장
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
