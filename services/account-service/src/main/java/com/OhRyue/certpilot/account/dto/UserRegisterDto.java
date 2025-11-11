17f43455d7e48494e6c0e914e97e572174455821
ohyerim025-alt <ohyerim025@gmail.com>
2025-11-11 15:39:16 +0900

    feat: 비밀번호 형식 확인

--- services/account-service/src/main/java/com/OhRyue/certpilot/account/dto/UserRegisterDto.java
+++ services/account-service/src/main/java/com/OhRyue/certpilot/account/dto/UserRegisterDto.java
@@ -9,6 +9,10 @@ import lombok.Setter;
 public class UserRegisterDto {
     @Pattern(regexp = "^[A-Za-z0-9]{8,20}$", message = "아이디는 영문과 숫자 8~20자여야 합니다.")
     private String username;
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 포함한 8자 이상이어야 합니다."
+    )
     private String password;
     private String email;
 }
